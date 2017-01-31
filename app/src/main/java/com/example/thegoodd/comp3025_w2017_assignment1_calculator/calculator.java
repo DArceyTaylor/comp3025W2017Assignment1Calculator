1c0192d53f3bf3de747323b39a9a22f152af7293
D'Arcey <darceytaylor@gmail.com>
2017-01-31 17:55:25 -0500

    Final Push:  added  more comments, removed excess code and variables

--- app/src/main/java/com/example/thegoodd/comp3025_w2017_assignment1_calculator/calculator.java
+++ app/src/main/java/com/example/thegoodd/comp3025_w2017_assignment1_calculator/calculator.java
@@ -19,11 +19,10 @@ import android.widget.TextView;
 public class calculator extends Activity implements View.OnClickListener {
 
     //variables
-    Button one,two,three,four,five,six,seven,eight,nine,zero,addition,subtraction,division,multiplication,clear_everything,clear,equals,decimal,sign,backspace,left_bracket,right_bracket,divisionDouble,squared,squared_root;
+    Button one,two,three,four,five,six,seven,eight,nine,zero,addition,subtraction,division,multiplication,clear_everything,clear,equals,decimal,sign,backspace;
     TextView result;
-    int value1, value2;
-    double value3, value4;
-    String operation, answer;
+    double value1, value2;
+    String operation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,13 +52,7 @@ public class calculator extends Activity implements View.OnClickListener {
         decimal = (Button) findViewById(R.id.decimal);
         backspace = (Button) findViewById(R.id.backspace);
         sign = (Button) findViewById(R.id.sign);
-        /*
-        left_bracket = (Button) findViewById(R.id.left_bracket);
-        right_bracket = (Button) findViewById(R.id.right_bracket);
-        pi = (Button) findViewById(R.id.pi);
-        squared = (Button) findViewById(R.id.squared);
-        squared_root = (Button) findViewById(R.id.squared_root);
-*/
+
         //Text Views
         result = (TextView) findViewById(R.id.result);
 
@@ -84,309 +77,299 @@ public class calculator extends Activity implements View.OnClickListener {
         decimal.setOnClickListener(this);
         backspace.setOnClickListener(this);
         sign.setOnClickListener(this);
-        /*
-        left_bracket.setOnClickListener(this);
-        right_bracket.setOnClickListener(this);
-        pi.setOnClickListener(this);
-        squared.setOnClickListener(this);
-        squared_root.setOnClickListener(this);
-*/
 
         result.setText("0");
     }
 
-    //This  is a method where all the calculations will take place
+    //This  is a method where all the calculations will take place depending on the operation that was selected
     public void math(){
         //addition
         if(operation.equals("+")){
-            value4 = Double.parseDouble(result.getText().toString());
+            value2 = Double.parseDouble(result.getText().toString());
             result.setText("");
-            value3 = value3 + value4;
-            result.setText(Double.toString(value3));
+            value1 = value1 + value2;
+            result.setText(Double.toString(value1));
         }
         //subtraction
         else if(operation.equals("-")){
-            value4 = Double.parseDouble(result.getText().toString());
+            value2 = Double.parseDouble(result.getText().toString());
             result.setText("");
-            value3 = value3 - value4;
-            result.setText(Double.toString(value3));
+            value1 = value1 - value2;
+            result.setText(Double.toString(value1));
         }
         //multiplication
         else if(operation.equals("x")){
-            value4 = Double.parseDouble(result.getText().toString());
+            value2 = Double.parseDouble(result.getText().toString());
             result.setText("");
-            value3 = value3 * value4;
-            result.setText(Double.toString(value3));
+            value1 = value1 * value2;
+            result.setText(Double.toString(value1));
         }
         //division
         else if(operation.equals("/")){
-            value4 = Double.parseDouble(result.getText().toString());
-            if(value4 == 0.0) {
+            value2 = Double.parseDouble(result.getText().toString());
+            if(value2 == 0.0) {
                 result.setTextSize(36);
                 result.setText("Cannot Divide by zero");
-                value3 = 0;
-                value4 = 0;
+                value1 = 0;
+                value2 = 0;
             }
             else
                 result.setText("");
-                value3 = value3 / value4;
-                result.setText(Double.toString(value3));
+                value1 = value1 / value2;
+                result.setText(Double.toString(value1));
         }
     }
 
     public void onClick(View v){
         //variables
-        String string = result.getText().toString();
+        String currentNumber = result.getText().toString();
 
         //Switch case to determine which button was pressed and stores that value into variables value1 & value2
         //also outputs the value of the button to the screen
         switch (v.getId()){
             case R.id.one:
-                if(value4 !=0){
-                    value4 = 0;
+                if(value2 !=0){
+                    value2 = 0;
                     result.setText("");
                 }
-                else if (string == "0")
+                else if (currentNumber == "0")
                     result.setText(one.getText());
                 else
-                    result.setText(string + one.getText());
+                    result.setText(currentNumber + one.getText());
                 break;
+
             case R.id.two:
-
-
-                if(value4 !=0){
-                    value4 = 0;
+                if(value2 !=0){
+                    value2 = 0;
                     result.setText("");
                 }
-                else if (string == "0")
+                else if (currentNumber == "0")
                     result.setText(two.getText());
                 else
-                    result.setText(string + two.getText());
+                    result.setText(currentNumber + two.getText());
                 break;
-            case R.id.three:
 
-                 if(value4 !=0){
-                    value4 = 0;
+            case R.id.three:
+                 if(value2 !=0){
+                     value2 = 0;
                     result.setText("");
                 }
-                else if (string == "0")
+                else if (currentNumber == "0")
                     result.setText(three.getText());
                 else
-                    result.setText(string + three.getText());
+                    result.setText(currentNumber + three.getText());
                 break;
-            case R.id.four:
 
-                 if(value4 !=0){
-                    value4 = 0;
+            case R.id.four:
+                 if(value2 !=0){
+                     value2 = 0;
                     result.setText("");
                 }
-                else if (string == "0")
+                else if (currentNumber == "0")
                     result.setText(four.getText());
                 else
-                    result.setText(string + four.getText());
+                    result.setText(currentNumber + four.getText());
                 break;
-            case R.id.five:
 
-                 if(value4 !=0){
-                    value4 = 0;
+            case R.id.five:
+                 if(value2 !=0){
+                     value2 = 0;
                     result.setText("");
                 }
-                else if (string == "0")
+                else if (currentNumber == "0")
                     result.setText(five.getText());
                 else
-                    result.setText(string + five.getText());
+                    result.setText(currentNumber + five.getText());
                 break;
-            case R.id.six:
 
-                 if(value4 !=0){
-                    value4 = 0;
+            case R.id.six:
+                 if(value2 !=0){
+                     value2 = 0;
                     result.setText("");
                 }
-                else if (string == "0")
+                else if (currentNumber == "0")
                     result.setText(six.getText());
                 else
-                    result.setText(string + six.getText());
+                    result.setText(currentNumber + six.getText());
                 break;
-            case R.id.seven:
 
-                 if(value4 !=0){
-                    value4 = 0;
+            case R.id.seven:
+                 if(value2 !=0){
+                     value2 = 0;
                     result.setText("");
                 }
-                else if (string == "0")
+                else if (currentNumber == "0")
                     result.setText(seven.getText());
                 else
-                    result.setText(string + seven.getText());
+                    result.setText(currentNumber + seven.getText());
                 break;
-            case R.id.eight:
 
-                 if(value4 !=0){
-                    value4 = 0;
+            case R.id.eight:
+                 if(value2 !=0){
+                     value2 = 0;
                     result.setText("");
                 }
-                else if (string == "0")
+                else if (currentNumber == "0")
                     result.setText(eight.getText());
                 else
-                    result.setText(string + eight.getText());
+                    result.setText(currentNumber + eight.getText());
                 break;
-            case R.id.nine:
 
-                 if(value4 !=0){
-                    value4 = 0;
+            case R.id.nine:
+                 if(value2 !=0){
+                     value2 = 0;
                     result.setText("");
                 }
-                else if (string == "0")
+                else if (currentNumber == "0")
                     result.setText(nine.getText());
                 else
-                    result.setText(string + nine.getText());
+                    result.setText(currentNumber + nine.getText());
                 break;
-            case R.id.zero:
 
-                 if(value4 !=0){
-                    value4 = 0;
+            case R.id.zero:
+                 if(value2 !=0){
+                     value2 = 0;
                     result.setText("");
                 }
-                else if (string == "0")
+                else if (currentNumber == "0")
                     result.setText(zero.getText());
                 else
-                    result.setText(string + zero.getText());
+                    result.setText(currentNumber + zero.getText());
                 break;
 
             //clears the most recently input number and sets back to zero
             case R.id.clear:
                 if(result.getText().toString() != ""){
-                    value3 = Double.parseDouble(result.getText().toString());
-                    value4 = 0;
+                    value1 = Double.parseDouble(currentNumber);
+                    value2 = 0;
                     result.setText("");
                 }
-                value4 = 0;
+                value2 = 0;
                 result.setText("");
                 break;
+
             //clears the entire memory of input numbers
             case R.id.clear_everything:
-                //value1 = 0;
-                //value2 = 0;
-                value3 = 0;
-                value4 = 0;
+                value1 = 0;
+                value2 = 0;
                 result.setText("");
                 break;
 
             case R.id.addition:
                 operation = "+";
 
-                if(value3 == 0){
-                    value3 = Double.parseDouble(result.getText().toString());
+                if(value1 == 0){
+                    value1 = Double.parseDouble(currentNumber);
                     result.setText("");
                 }
-                else if(value4 != 0){
-                    value4 = 0;
+                else if(value2 != 0){
+                    value2 = 0;
                     result.setText("");
                 }
                 else{
-                    value4 = Double.parseDouble(result.getText().toString());
+                    value2 = Double.parseDouble(currentNumber);
                     result.setText("");
-                    value3 = value3 + value4;
-                    if(value3 < 0)
-                        result.setText("-" + value3);
+                    value1 = value1 + value2;
+                    if(value1 < 0)
+                        result.setText("-" + value1);
                     else
-                    result.setText(Double.toString(value3));
+                    result.setText(Double.toString(value1));
                 }
                 break;
 
             case R.id.subtraction:
                 operation = "-";
 
-                if(value3 == 0){
-                    value3 = Double.parseDouble(result.getText().toString());
+                if(value1 == 0){
+                    value1 = Double.parseDouble(currentNumber);
                     result.setText("");
                 }
-                else if(value4 != 0){
-                    value4 = 0;
+                else if(value2 != 0){
+                    value2 = 0;
                     result.setText("");
                 }
                 else{
-                    value4 = Double.parseDouble(result.getText().toString());
+                    value2 = Double.parseDouble(currentNumber);
                     result.setText("");
-                    value3 = value3 - value4;
-                    if(value3 < 0)
-                        result.setText("-" + Double.toString(value3));
+                    value1 = value1 - value2;
+                    if(value1 < 0)
+                        result.setText("-" + Double.toString(value1));
                     else
-                    result.setText(Double.toString(value3));
+                    result.setText(Double.toString(value1));
                 }
                 break;
 
             case R.id.multiplication:
                 operation = "x";
 
-                if(value3 == 0){
-                    value3 = Double.parseDouble(result.getText().toString());
+                if(value1 == 0){
+                    value1 = Double.parseDouble(currentNumber);
                     result.setText("");
                 }
-                else if(value4 != 0){
-                    value4 = 0;
+                else if(value2 != 0){
+                    value2 = 0;
                     result.setText("");
                 }
                 else{
-                    value4 = Double.parseDouble(result.getText().toString());
+                    value2 = Double.parseDouble(currentNumber);
                     result.setText("");
-                    value3 = value3 * value4;
-                    if(value3 < 0)
-                        result.setText("-" + Double.toString(value3));
+                    value1 = value1 * value2;
+                    if(value1 < 0)
+                        result.setText("-" + Double.toString(value1));
                     else
-                    result.setText(Double.toString(value3));
+                    result.setText(Double.toString(value1));
                 }
                 break;
 
             case R.id.division:
                 operation = "/";
 
-                if(value3 == 0){
-                    value3 = Double.parseDouble(result.getText().toString());
+                if(value1 == 0){
+                    value1 = Double.parseDouble(currentNumber);
                     result.setText("");
                 }
-                else if(value4 != 0){
-                    value4 = 0;
+                else if(value2 != 0){
+                    value2 = 0;
                     result.setText("");
                 }
                 else{
-                    value4 = Double.parseDouble(result.getText().toString());
+                    value2 = Double.parseDouble(currentNumber);
                     result.setText("");
-                    value3 = value3 - value4;
-                    if(value3 < 0)
-                        result.setText("-" + Double.toString(value3));
+                    value1 = value1 - value2;
+                    if(value1 < 0)
+                        result.setText("-" + Double.toString(value1));
                     else
-                        result.setText(Double.toString(value3));
+                        result.setText(Double.toString(value1));
                 }
                 break;
 
             //displays the solution that was computed
             case R.id.equals:
                 if(!operation.equals(null)){
-                    if(value4 != 0) {
+                    if(value2 != 0) {
                         if (operation.equals("+")) {
                             result.setText("");
-                            result.setText(Double.toString(value3));
-                            value3 = Double.parseDouble(result.getText().toString());
-                            value4 = 0;
+                            result.setText(Double.toString(value1));
+                            value1 = Double.parseDouble(currentNumber);
+                            value2 = 0;
                         }
                         if (operation.equals("-")) {
                             result.setText("");
-                            result.setText(Double.toString(value3));
-                            answer = result.getText().toString();
-                            value3 = 0;
-                            value4 = 0;
+                            result.setText(Double.toString(value1));
+                            value1 = Double.parseDouble(currentNumber);
+                            value2 = 0;
                         }
                         if (operation.equals("x")) {
                             result.setText("");
-                            result.setText(Double.toString(value3));
-                            value3 = Double.parseDouble(result.getText().toString());
-                            value4 = 0;
+                            result.setText(Double.toString(value1));
+                            value1 = Double.parseDouble(currentNumber);
+                            value2 = 0;
                         }
                         if (operation.equals("/")) {
                             result.setText("");
-                            result.setText(Double.toString(value3));
-                            value3 = Double.parseDouble(result.getText().toString());
-                            value4 = 0;
+                            result.setText(Double.toString(value1));
+                            value1 = Double.parseDouble(currentNumber);
+                            value2 = 0;
                         }
                     }
                     else
@@ -407,24 +390,24 @@ public class calculator extends Activity implements View.OnClickListener {
                 if(result.getText().toString().contains("."))
                     result.setText(result.getText());
                 else {
-                    if (value4 != 0) {
-                        value4 = 0;
+                    if (value2 != 0) {
+                        value2 = 0;
                         result.setText("");
                     }
                     else if (result.getText() == "")
                         result.setText("0" + decimal.getText());
                     else
-                        result.setText(string + decimal.getText());
+                        result.setText(currentNumber + decimal.getText());
                 }
                 break;
             //adds a negative to the value
             case R.id.sign:
-                if (value4 != 0) {
-                    value4 = 0;
+                if (value2 != 0) {
+                    value2 = 0;
                     result.setText("");
                 }
-                else if(Double.parseDouble(result.getText().toString()) > 0){
-                    result.setText("-" + string);
+                else if(Double.parseDouble(currentNumber) > 0){
+                    result.setText("-" + currentNumber);
                 }
                 else
                 result.getText();
